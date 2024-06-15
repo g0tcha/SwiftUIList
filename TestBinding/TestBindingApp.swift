6a81e075b729c63e3d3fdbcd8e5adfa01987ba61
Vincent Grossier <vincent.grossier@gmail.com>
2024-06-14 23:06:14 -0400

    Initial Commit

---
+++ TestBinding/TestBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBindingApp.swift
+//  TestBinding
+//
+//  Created by Vincent Grossier on 2024-06-14.
+//
+
+import SwiftUI
+
+@main
+struct TestBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
